4baeb4f60ba4213d2fbe2ee1b61aada5e719ea40
Seth-McKilla <seth@endevrs.dev>
2022-01-15 22:25:27 -0500

    Add placeholder NFT for NFT list

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   images: {
     domains: ["prod-metadata.s3.amazonaws.com", "ethereum.org"],
   },
+  images: {
+    domains: ["cdn.pixabay.com", "prod-metadata.s3.amazonaws.com"],
+  },
 };
--- frontend/pages/nfts/index.tsx
+++ frontend/pages/nfts/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { NextPage } from "next";
 import { Layout } from "../../components";
 
@@ -14,32 +15,57 @@ interface Props {
 
 const Home: NextPage<Props> = (props) => {
   const { nfts, error } = props;
-  console.log(nfts);
 
   return (
     <Layout>
       <div className="grid p-32">
         <h1 className="grid text-4xl place-items-center">{"My NFT's"}</h1>
-        {nfts?.map((nft, idx) => (
-          <div key={`${idx}`}>Placeholder</div>
-        ))}
+        <div className="grid grid-cols-1 gap-5 p-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+          {nfts?.map((nft, idx) => (
+            <div
+              key={`${idx}-${nft.name}`}
+              className="overflow-hidden rounded-md shadow-xl"
+            >
+              <Image
+                src={nft.image}
+                alt={nft.name}
+                width="100%"
+                height="100%"
+                layout="responsive"
+                objectFit="contain"
+              />
+              <div className="px-2 py-2">
+                <div className="mb-2 text-xl font-bold">{nft.name}</div>
+                <p className="text-base text-gray-700">{nft.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
-  let nfts = [];
+  let nfts = [
+    {
+      name: "Mountains",
+      description:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+      image:
+        "https://cdn.pixabay.com/photo/2021/11/16/18/10/nature-6801719_960_720.jpg",
+    },
+  ];
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/nfts`);
     nfts = await response.json();
 
     return {
-      props: { nfts },
+      props: { nfts, error: null },
     };
   } catch (error: any) {
     console.error(error);
-    return { props: { error: error?.message } };
+    return { props: { nfts, error: error?.message } };
   }
 }
 
