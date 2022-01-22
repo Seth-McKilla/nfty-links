88d81fba1c9d86ae95f0445f405aa603418d49fb
Seth-McKilla <seth@endevrs.dev>
2022-01-22 18:17:34 -0500

    feat: replace login with connect on homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,68 +1,22 @@
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
-import { useAccount, useSignMessage } from "wagmi";
+import { useState } from "react";
+import { useAccount } from "wagmi";
 import { Layout, Loader, Button } from "../components";
-import useAuthLogin from "../hooks/useAuthLogin";
-import { ImportOptionsModal, Modal } from "../components";
-
-const { NEXT_PUBLIC_API_URL } = process.env;
+import { ImportOptionsModal, WalletOptionsModal } from "../components";
 
 const Home: NextPage = () => {
   // Hooks
   const [{ data: accountData, error: accountError, loading: accountLoading }] =
     useAccount();
-  const { text, loading: authLoading } = useAuthLogin(accountData?.address);
-  const [_, signMessage] = useSignMessage();
 
   // Local State
-  const [loginLoading, setLoginLoading] = useState<boolean>(false);
-  const [loginError, setLoginError] = useState<string>("");
-  const [authToken, setAuthToken] = useState<string>("");
   const [showMarketplacesModal, setShowMarketplacesModal] =
     useState<boolean>(false);
-  const [showConnectModal, setShowConnectModal] = useState<boolean>(false);
-
-  const getAuthToken = async () => {
-    if (!accountData?.address) return setShowConnectModal(true);
-    setLoginLoading(true);
-
-    try {
-      const { data: sig } = await signMessage({ message: text });
-      const response = await fetch(`${NEXT_PUBLIC_API_URL}auth/token`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          address: accountData?.address,
-          sig,
-        }),
-      });
-      const token = await response.text();
-
-      localStorage.setItem("token", token);
-      setAuthToken(token);
-    } catch (error: any) {
-      console.error(error);
-      setLoginError(error?.message);
-    }
-
-    return setLoginLoading(false);
-  };
-
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    token && setAuthToken(token.toString());
-  }, [accountData?.address]);
-
-  const logout = () => {
-    localStorage.removeItem("token");
-    setAuthToken("");
-  };
+  const [showWalletOptions, setShowWalletOptions] = useState<boolean>(false);
 
   const renderContent = () => {
-    if (accountLoading || authLoading) return <Loader size={8} />;
+    if (accountLoading) return <Loader size={8} />;
 
     if (accountError)
       return (
@@ -79,17 +33,8 @@ const Home: NextPage = () => {
           Embed an NFT into a url and send it to anyone
         </h3>
         <div className="flex">
-          {authToken ? (
+          {accountData?.address ? (
             <div className="flex">
-              <div className="mr-6">
-                <Button
-                  onClick={() => {
-                    logout();
-                  }}
-                >
-                  {"Logout"}
-                </Button>
-              </div>
               <div className="mr-6">
                 <Link href="/nfts" passHref>
                   <a>
@@ -113,16 +58,13 @@ const Home: NextPage = () => {
           ) : (
             <div className="mr-6">
               <Button
-                loading={loginLoading}
-                onClick={() => {
-                  getAuthToken();
-                }}
+                loading={showWalletOptions}
+                onClick={() => setShowWalletOptions(true)}
               >
-                {"Login"}
+                Connect
               </Button>
             </div>
           )}
-          {loginError && <p className="text-sm text-red-500">{loginError}</p>}
         </div>
       </>
     );
@@ -136,23 +78,11 @@ const Home: NextPage = () => {
           setOpen={setShowMarketplacesModal}
         />
       )}
-      {showConnectModal && (
-        <Modal open={showConnectModal}>
-          <div className="p-4 text-center">
-            <h1 className="mb-4 text-4xl font-bold">Connect Wallet</h1>
-            <p className="text-xl">Please connect your wallet to continue.</p>
-            <div className="flex items-center justify-end p-3 mt-1">
-              <button
-                className="px-6 py-2 mb-1 text-sm font-semibold text-red-500 uppercase"
-                type="button"
-                onClick={() => setShowConnectModal(false)}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </Modal>
-      )}
+
+      <WalletOptionsModal
+        open={showWalletOptions}
+        setOpen={setShowWalletOptions}
+      />
 
       <div className="grid h-screen place-items-center">
         <div className="grid place-items-center">{renderContent()}</div>
