c1aa5f8c25a144f97fac0fcb25753349fb511269
Seth McCullough <seth@endevrs.dev>
2022-01-31 06:45:43 -0500

    Setup context

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAccount, useSignMessage } from "wagmi";
 import { Layout, Loader, Button } from "../components";
 import useAuthLogin from "../hooks/useAuthLogin";
@@ -10,21 +9,17 @@ const { NEXT_PUBLIC_API_URL } = process.env;
 
 const Home: NextPage = () => {
   // Hooks
-  const [{ data: accountData, error: accountError, loading: accountLoading }] =
-    useAccount();
-  const { text, loading: authLoading } = useAuthLogin(accountData?.address);
+  const [{ data, error, loading }] = useAccount();
+  const { text, loading: authLoading } = useAuthLogin(data?.address);
   const [_, signMessage] = useSignMessage();
 
   // Local State
   const [loginLoading, setLoginLoading] = useState<boolean>(false);
   const [loginError, setLoginError] = useState<string>("");
-  const [authToken, setAuthToken] = useState<string>("");
   const [showMarketplacesModal, setShowMarketplacesModal] =
     useState<boolean>(false);
-  const [showConnectModal, setShowConnectModal] = useState<boolean>(false);
 
   const getAuthToken = async () => {
-    if (!accountData?.address) return setShowConnectModal(true);
     setLoginLoading(true);
 
     try {
@@ -35,14 +30,13 @@ const Home: NextPage = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          address: accountData?.address,
+          address: data?.address,
           sig,
         }),
       });
       const token = await response.text();
 
       localStorage.setItem("token", token);
-      setAuthToken(token);
     } catch (error: any) {
       console.error(error);
       setLoginError(error?.message);
@@ -51,24 +45,14 @@ const Home: NextPage = () => {
     return setLoginLoading(false);
   };
 
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    token && setAuthToken(token.toString());
-  }, [accountData?.address]);
-
-  const logout = () => {
-    localStorage.removeItem("token");
-    setAuthToken("");
-  };
-
   const renderContent = () => {
-    if (accountLoading || authLoading) return <Loader size={8} />;
+    if (loading || authLoading) return <Loader size={8} />;
 
-    if (accountError)
+    if (error)
       return (
         <>
           <h1 className="mb-3 text-4xl">Oops!</h1>
-          <p className="text-xl text-red-500">{`Error: ${accountError?.message}`}</p>
+          <p className="text-xl text-red-500">{`Error: ${error?.message}`}</p>
         </>
       );
 
@@ -79,49 +63,16 @@ const Home: NextPage = () => {
           Embed an NFT into a url and send it to anyone
         </h3>
         <div className="flex">
-          {authToken ? (
-            <div className="flex">
-              <div className="mr-6">
-                <Button
-                  onClick={() => {
-                    logout();
-                  }}
-                >
-                  {"Logout"}
-                </Button>
-              </div>
-              <div className="mr-6">
-                <Link href="/nfts" passHref>
-                  <a>
-                    <Button>{"View NFT's"}</Button>
-                  </a>
-                </Link>
-              </div>
-              <div className="mr-6">
-                <Link href="/nfts/create" passHref>
-                  <a>
-                    <Button>Create NFT</Button>
-                  </a>
-                </Link>
-              </div>
-              <div>
-                <Button onClick={() => setShowMarketplacesModal(true)}>
-                  Import NFT
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <div className="mr-6">
-              <Button
-                loading={loginLoading}
-                onClick={() => {
-                  getAuthToken();
-                }}
-              >
-                {"Login"}
-              </Button>
-            </div>
-          )}
+          <div className="mr-6">
+            <Button
+              loading={loginLoading}
+              onClick={() => {
+                getAuthToken();
+              }}
+            >
+              {"Login"}
+            </Button>
+          </div>
           {loginError && <p className="text-sm text-red-500">{loginError}</p>}
         </div>
       </>
@@ -135,23 +86,6 @@ const Home: NextPage = () => {
           open={showMarketplacesModal}
           setOpen={setShowMarketplacesModal}
         />
-      )}
-      {showConnectModal && (
-        <Modal open={showConnectModal}>
-          <div className="p-4 text-center">
-            <h1 className="mb-4 text-4xl font-bold">Connect Wallet</h1>
-            <p className="text-xl">Please connect your wallet to continue.</p>
-            <div className="flex items-center justify-end p-3 mt-1">
-              <button
-                className="px-6 py-2 mb-1 text-sm font-semibold text-red-500 uppercase"
-                type="button"
-                onClick={() => setShowConnectModal(false)}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </Modal>
       )}
 
       <div className="grid h-screen place-items-center">
